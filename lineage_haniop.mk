bb28c0a89337a3a05012e849a0694a6844e508d7
bhawesh2002 <bhaweshmankar2002@gmail.com>
2021-10-28 10:49:47 +0530

    some minor changes

--- lineage_haniop.mk
+++ lineage_haniop.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-
+ 
 $(call inherit-product, device/motorola/haniop/device.mk)
 
 # Inherit some common Lineage stuff.
