d2580ed4fbc060885d735a9b7cabe19fa7e54f31
bhawesh2002 <bhaweshmankar2002@gmail.com>
2021-10-28 10:23:47 +0530

    some minor changes

--- lineage_haniop.mk
+++ lineage_haniop.mk
@@ -1,31 +1,21 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from haniop device
-#$(call inherit-product, device/motorola/haniop/device.mk)
+#TODO: add this file
+#$(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := hanoip_retail
+PRODUCT_NAME := lineage_hanoip_retail
 PRODUCT_DEVICE := hanoip
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(40) fusion
@@ -38,7 +28,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
-#TODO: update fingerprint.
 # Build info
 BUILD_FINGERPRINT := "motorola/hanoip_retail/hanoip:11/RRI31.Q1-42-48/222fd:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
