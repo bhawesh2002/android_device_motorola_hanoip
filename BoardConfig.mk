9f79d575195e888f6397d62dd3203996b62602b6
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2021-10-31 23:10:16 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,27 +10,27 @@ TARGET_SCREEN_DENSITY := 395
 # Inherit from sm6150-common
 -include device/motorola/sm6150-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/motorola/haniop
+DEVICE_PATH := device/motorola/hanoip
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_haniop
-TARGET_RECOVERY_DEVICE_MODULES := libinit_haniop
+TARGET_INIT_VENDOR_LIB := libinit_hanoip
+TARGET_RECOVERY_DEVICE_MODULES := libinit_hanoip
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 ODM_MANIFEST_SKUS += \
-    haniop
+    hanoip
 
-ODM_MANIFEST_HANIOP_FILES := $(DEVICE_PATH)/manifest_haniop.xml
+ODM_MANIFEST_HANOIP_FILES := $(DEVICE_PATH)/manifest_hanoip.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
-TARGET_KERNEL_CONFIG := vendor/lineage_haniop_defconfig
+TARGET_KERNEL_CONFIG := vendor/lineage_hanoip_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
