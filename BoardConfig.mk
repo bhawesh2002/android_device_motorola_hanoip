804e2f3d32f8d08029bacac57cc6548a89042449
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-01-13 00:07:17 +0530

    hanoip: add architecture details

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,24 @@
 # Display density
 TARGET_SCREEN_DENSITY := 400
 
+#Path to device
 DEVICE_PATH := device/motorola/hanoip
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
