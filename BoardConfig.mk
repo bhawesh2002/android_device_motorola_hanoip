f26eeaa6234bd5d806118543c3a2dfe5d4455349
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-01-13 00:16:38 +0530

    hanoip : add kernel details

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,3 +42,16 @@ ENABLE_SCHEDBOOST := true
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 printk.devkmsg=on androidboot.hab.csv=5 androidboot.hab.product=hanoip androidboot.hab.cid=50 firmware_class.path=/vendor/firmware_mnt/image buildvariant=user
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/motorola/hanoip
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r416183c1
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
