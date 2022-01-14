eb50dde9cadf996e3607f3bb88f0e0cd90c3ac58
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-01-14 14:31:25 +0530

    hanoip: add telephoney

--- device.mk
+++ device.mk
@@ -770,3 +770,17 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+
