a760e3cbf182be7969824b96d6510caa52b042fe
bhawesh2002 <bhaweshmankar2002@gmail.com>
2022-04-05 03:30:28 +0530

    create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The aospOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_hanoip.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_hanoip-user \
+    aosp_hanoip-userdebug \
+    aosp_hanoip-eng
